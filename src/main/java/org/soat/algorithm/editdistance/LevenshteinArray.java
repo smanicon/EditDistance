58a1b241fe31e1b4fab0c3f0713076557527729f
SManicon <sebastien.manicon@soat.fr>
2013-02-18 15:17:19 +0100

    Implements the calculate cell function.
    This method is a part of the Levenshtein algorithm. To complete the algorithm, an other method must call this methods for all cells.

--- src/main/java/org/soat/algorithm/editdistance/LevenshteinArray.java
+++ src/main/java/org/soat/algorithm/editdistance/LevenshteinArray.java
@@ -65,4 +65,36 @@ public class LevenshteinArray {
 
         return x + 1;
     }
+
+    /**
+     * @see LevenshteinArray#calculateCellValue(int, int, boolean)
+     *
+     * @param x the x coordinate of the array.
+     * @param y the y coordinate of the array.
+     * @param c1 char to test.
+     * @param c2 char to test.
+     *
+     */
+    public void calculateCellValue(int x, int y, char c1, char c2) {
+        calculateCellValue(x, y, c1 == c2);
+    }
+
+    /**
+     * Apply the Levenshtein calculate on a cell. Test on previews cell
+     * if there are an insertion, a suppression or a substitution.
+     * To calculate a cell, make sur the previous cells ( [x-1, y], [x, y-1]
+     * and [x-1, y-1] ) has been already calculated.
+     *
+     * @param x the x coordinate of the array.
+     * @param y the y coordinate of the array.
+     * @param isSame
+     */
+    public void calculateCellValue(int x, int y, boolean isSame) {
+        int insertion    = this.getCellValue(x-1, y) + 1;
+        int suppression  = this.getCellValue(x, y-1) + 1;
+        int substitution = this.getCellValue(x-1, y-1) + (isSame ? 0 : 1);
+
+        this.setCellValue(x, y, Math.min(insertion, Math.min(suppression, substitution)));
+    }
+
 }
--- src/test/java/org/soat/algorithm/editdistance/LevenshteinArrayTest.java
+++ src/test/java/org/soat/algorithm/editdistance/LevenshteinArrayTest.java
@@ -17,4 +17,47 @@ public class LevenshteinArrayTest {
         assertThat(la.getCellValue(-1,  9)).isEqualTo(10);
         assertThat(la.getCellValue( 9, -1)).isEqualTo(10);
     }
+
+    @Test
+    public void testCellCalculateSubstitutionNotSame() {
+        LevenshteinArray la = new LevenshteinArray(10, 10);
+
+        la.calculateCellValue(0, 0, false);
+        assertThat(la.getCellValue(0, 0)).isEqualTo(1);
+    }
+
+    @Test
+    public void testCellCalculateSubstitutionSame() {
+        LevenshteinArray la = new LevenshteinArray(10, 10);
+
+        la.calculateCellValue(0, 0, true);
+        assertThat(la.getCellValue(0, 0)).isEqualTo(0);
+    }
+
+    @Test
+    public void testCellCalculateInsertion() {
+        LevenshteinArray la = new LevenshteinArray(10, 10);
+
+        la.setCellValue(0, 0, 3);
+        la.setCellValue(1, 0, 3);
+        la.setCellValue(0, 1, 1);
+
+        la.calculateCellValue(1, 1, true);
+        assertThat(la.getCellValue(1, 1)).isEqualTo(2);
+    }
+
+    @Test
+    public void testCellCalculateSuppression() {
+        LevenshteinArray la = new LevenshteinArray(10, 10);
+
+        la.setCellValue(0, 0, 3);
+        la.setCellValue(1, 0, 3);
+        la.setCellValue(0, 1, 1);
+
+        la.calculateCellValue(1, 1, true);
+        assertThat(la.getCellValue(1, 1)).isEqualTo(2);
+    }
+
+
+
 }
